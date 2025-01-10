62ea632cfbb01ed6a8c6229914bc94d7ee21f2c8
Ilya <ilya.torgaykin@mail.ru>
2025-01-10 08:37:55 +0400

    fix bugs with price

--- shared/lib/calc-cart-item-total-price.ts
+++ shared/lib/calc-cart-item-total-price.ts
@@ -2,6 +2,11 @@ import { CartItemDTO } from '../services/dto/cart.dto';
 
 export const calcCartItemTotalPrice = (item: CartItemDTO): number => {
   const ingredientsPrice = item.ingredients.reduce((acc, ingredient) => acc + ingredient.price, 0);
-
-  return (ingredientsPrice) * item.quantity;
-};
+  console.log(ingredientsPrice);
+  console.log(item.productItem.price);
+  if (ingredientsPrice === 0) {
+    return (ingredientsPrice + item.productItem.price) * item.quantity;
+  } else {
+    return (ingredientsPrice) * item.quantity;
+  }
+}
